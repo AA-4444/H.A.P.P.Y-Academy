2f1d8c0336f64d66da9a7bd719823d0564a154b9
Oleksii Zarytskyi <104495043+AA-4444@users.noreply.github.com>
2026-03-06 12:03:09 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={window.location.pathname.startsWith("/quiz") ? "/quiz" : "/"}>
         <Routes>
           <Route path="/" element={<Index />} />
        
